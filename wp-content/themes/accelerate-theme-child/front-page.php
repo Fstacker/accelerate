e4804978d7ff83a2f90a22413aa72a52ad46f39e
fstacker <brett@yutster.com>
2019-08-19 21:35:08 -0600

    social_media_1

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -13,26 +13,57 @@
  */
 
 get_header(); ?>
-
+<!-- <pre><?php //print_r($wp_query); exit; ?></pre> -->
+	
 	<div id="primary" class="home-page hero-content">
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
-				<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
+				<a class="button" href="<?php echo site_url('/case-studies/') ?>">View Our Work</a>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
 
+<!--  FEATURED WORK  -->
+<section id="fw-home" class="featured-work">
+	<div class="site-content">
+		
+		<h4>Featured Work</h4>
+
+		<ul>
+			<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+				<?php while (have_posts()) : the_post();
+					$image_1 = get_field('image_1');
+					$size = "medium"
+				?>
+				<li class="featured-work">
+					<a href="<?php echo the_permalink(); ?>">
+						<figure>
+							<?php echo wp_get_attachment_image($image_1, $size); ?>
+						</figure>
+						<h6><?php the_title(); ?></h6>
+					</a>
+				</li>  <!-- end images -->
+			<? endwhile; ?>
+			<?php wp_reset_query(); ?>
+		</ul>
+	</div>
+</section> <!-- end FEATURED WORK -->
+
+
+<!-- RECENT BLOG POST -->
 	<section class="recent-posts">
 		<div class="site-content">
 			<div class="blog-post">
-				<h4>From the Blog</h4>
-				<?php query_posts('posts_per_page=1'); ?>
-					<?php while ( have_posts()) : the_post(); ?>
-						<h3><?php the_title(); ?></h3>
-						<?php the_excerpt(); ?>
-					<?php endwhile; ?>
-				<?php wp_reset_query(); ?>
+					
+					<h4>From the Blog</h4>
+					<?php query_posts('posts_per_page=1'); ?>
+					<!-- the loop -->
+						<?php while (have_posts() ) : the_post(); ?>
+							<h3><?php the_title(); ?></h3>
+							<?php the_excerpt(); ?>
+						<? endwhile; //end of the loop ?>
+					<?php wp_reset_query(); // resets the query back to its previous state ?>
 			</div> <!-- blog posts -->
 		</div> <!-- site content -->
 	</section> <!-- recent posts -->
--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -12,8 +12,29 @@
 
 
 // Enqueue scripts and styles
-function accelerate_child_scripts() {
+function accelerate_child_scripts(){
 	wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
+		wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
+			wp_enqueue_style('accelerate-child-fonts', '//fonts.googleapis.com/css?family=Heebo:400,500,700|Open+Sans:400,600|Roboto:400,500,700&display=swap');
 }
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
+
+// Function for post types
+function create_custom_post_types() {
+//	create a case study custom post type
+		register_post_type('case_studies',
+			array(
+				'labels' => array(
+						'name' => __( 'Case Studies' ),
+						'singular_name' => __( 'Case Study' )
+					),
+				'public' => true,
+				'has_archive' => true,
+				'rewrite' => array( 
+					'slug' => 'case-studies'
+				),
+			)
+		);
+}
+//	Hook this custom post type function into the theme
+add_action( 'init', 'create_custom_post_types' );
