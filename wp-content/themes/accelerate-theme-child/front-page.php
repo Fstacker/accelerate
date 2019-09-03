f52f59ee927b166c04ac0ecedd8d1cc69d275069
fstacker <brett@yutster.com>
2019-09-02 20:13:09 -0600

    contact us 1

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -30,7 +30,7 @@ get_header(); ?>
 		
 		<h4>Featured Work</h4>
 
-		<ul>
+		<ul class="featured-work">
 			<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
 				<?php while (have_posts()) : the_post();
 					$image_1 = get_field('image_1');
